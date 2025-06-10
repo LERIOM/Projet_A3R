53a686be594a48397a01a71d98baf2f2eee4c916
Lucavaa <lucavaa78@gmail.com>
2025-06-10 14:10:42 +0200

    histo type

--- CleaningData.R
+++ CleaningData.R
@@ -51,10 +51,6 @@ df <- df |> distinct()
 
 
 
-
-# (optionnel) Supprimer les lignes trop incomplètes
-df <- df |> drop_na(lat, lon, base_date_time, mmsi)
-
 # 6. Supprimer les valeurs aberrantes (exemples simples)
 df <- df |> 
   filter(
@@ -62,6 +58,7 @@ df <- df |>
     lon >= -98 & lon <= -78,
     sog <= 30,
     cog <= 360.0,
+    heading <= 360,
     )
 df <- subset(df, !(length == 0 | width == 0) | is.na(length) | is.na(width))
 
@@ -75,3 +72,15 @@ sum <- summary(vessel.total.clean)
 summary_df <- data.frame(valeur = sum)
 write_csv(summary_df, "summary.csv")
 
+hist(df$sog,
+     main = "Histogramme des vitesses des bateaux",
+     xlab = "Vitesse (sog)",
+     col = "skyblue",
+     border = "white",
+     breaks = 50)
+hist(df$vessel_type,
+     main = "Graphique des bateaux en fonction de leurs types",
+     xlab = "Type des bateaux (vessel_type)",
+     col = "skyblue",
+     border = 'white',
+     breaks = 50)
