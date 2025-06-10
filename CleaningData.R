98ce1693fadcf1b409816852fc3e8081af6d5c82
Lucavaa <lucavaa78@gmail.com>
2025-06-10 13:48:07 +0200

    nettoyage v3

--- CleaningData.R
+++ CleaningData.R
@@ -2,6 +2,9 @@ file<-read.csv("DossierdedonnéesBigDataNantes.csv")
 
 
 
+ 
+
+
 summary(file)
 str(file)
 head(file)
@@ -20,7 +23,18 @@ library(janitor)
 library(readr)
 
 # 1. Charger les données
-df <- file
+df <- vessel.total.clean
+# Liste des valeurs à convertir en NA
+valeurs_na <- c("", "NA", "\\N", "na", "n")
+
+# Appliquer le remplacement sur toutes les colonnes de type caractère ou facteur
+df <- as.data.frame(lapply(df, function(col) {
+  if (is.character(col) || is.factor(col)) {
+    col[col %in% valeurs_na] <- NA
+  }
+  return(col)
+}))
+
 # 2. Nettoyer les noms de colonnes
 df <- df |> clean_names()
 
@@ -35,8 +49,8 @@ df <- df |>
 # 4. Supprimer les doublons (identiques sur toutes les colonnes)
 df <- df |> distinct()
 
-# 5. Identifier les lignes avec valeurs manquantes
-missing_summary <- df |> summarise(across(everything(), ~sum(is.na(.))))
+
+
 
 # (optionnel) Supprimer les lignes trop incomplètes
 df <- df |> drop_na(lat, lon, base_date_time, mmsi)
@@ -46,16 +60,18 @@ df <- df |>
   filter(
     lat >= 20 & lat <= 30,
     lon >= -98 & lon <= -78,
-  )
+    sog <= 30,
+    cog <= 360.0,
+    )
+df <- subset(df, !(length == 0 | width == 0) | is.na(length) | is.na(width))
+
+# 5. Identifier les lignes avec valeurs manquantes
+missing_summary <- df |> summarise(across(everything(), ~sum(is.na(.))))
 
 # 7. Exporter la base propre
 write_csv(df, "vessel-total-clean-final.csv")
 
-hist(df$sog,
-     main = "Histogramme des vitesses des bateaux",
-     xlab = "Vitesse (sog)",
-     col = "skyblue",
-     border = "white",
-     breaks = 50)
-
+sum <- summary(vessel.total.clean)
+summary_df <- data.frame(valeur = sum)
+write_csv(summary_df, "summary.csv")
 
